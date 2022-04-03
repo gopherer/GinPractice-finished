dcdf043327dc93dfa3bc3c66fd62c6c8d8ffc738
gopher <97405569+gopherer@users.noreply.github.com>
2022-04-04 03:59:47 +0800

    Update UserRegisterController.go

--- controller/UserRegisterController.go
+++ controller/UserRegisterController.go
@@ -11,34 +11,6 @@ type UserRegisterController struct {
 }
 
 func (userRegister *UserRegisterController) UserRegisterController(context *gin.RouterGroup) {
-	context.POST("/register", userRegister.UserRegisterInstance)
-}
-
-func (userRegister *UserRegisterController) UserRegisterInstance(context *gin.Context) {
-	var userRegisterService service.UserRegisterService
-	result, err := userRegisterService.UserRegisterService(context)
-	if err != nil || result == 0 {
-		logger.Error("插入数据失败", err)
-		context.JSON(http.StatusOK, gin.H{
-			"message": "用户账号已被使用",
-		})
-	} else {
-		context.HTML(http.StatusOK, "user-register-success.html", gin.H{})
-	}
-}
-package controller
-
-import (
-	"github.com/gin-gonic/gin"
-	"github.com/wonderivan/logger"
-	"goWeb/service"
-	"net/http"
-)
-
-type UserRegisterController struct {
-}
-
-func (userRegister *UserRegisterController) UserRegisterController(context *gin.RouterGroup) {
 	context.POST("/register", userRegister.postUserRegister)
 	context.GET("register", userRegister.getUserRegister)
 }
@@ -53,7 +25,7 @@ func (userRegister *UserRegisterController) postUserRegister(context *gin.Contex
 		})
 	} else {
 		context.HTML(http.StatusOK, "user-register-success.html", gin.H{})
-	}
+	
 }
 func (userRegister *UserRegisterController) getUserRegister(context *gin.Context) {
 	context.HTML(http.StatusOK, "user-register.html", gin.H{})
