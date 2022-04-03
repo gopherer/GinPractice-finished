20536919661f276dc66da3887492866df7449ecd
gopher <97405569+gopherer@users.noreply.github.com>
2022-04-04 03:50:22 +0800

    Update UserRegisterController.go

--- controller/UserRegisterController.go
+++ controller/UserRegisterController.go
@@ -26,3 +26,35 @@ func (userRegister *UserRegisterController) UserRegisterInstance(context *gin.Co
 		context.HTML(http.StatusOK, "user-register-success.html", gin.H{})
 	}
 }
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/wonderivan/logger"
+	"goWeb/service"
+	"net/http"
+)
+
+type UserRegisterController struct {
+}
+
+func (userRegister *UserRegisterController) UserRegisterController(context *gin.RouterGroup) {
+	context.POST("/register", userRegister.postUserRegister)
+	context.GET("register", userRegister.getUserRegister)
+}
+
+func (userRegister *UserRegisterController) postUserRegister(context *gin.Context) {
+	var userRegisterService service.UserRegisterService
+	result, err := userRegisterService.UserRegisterService(context)
+	if err != nil || result == 0 {
+		logger.Error("插入数据失败", err)
+		context.JSON(http.StatusOK, gin.H{
+			"message": "用户账号已被使用",
+		})
+	} else {
+		context.HTML(http.StatusOK, "user-register-success.html", gin.H{})
+	}
+}
+func (userRegister *UserRegisterController) getUserRegister(context *gin.Context) {
+	context.HTML(http.StatusOK, "user-register.html", gin.H{})
+}
